60c498e074a184ccf46c777f4d46f5516e26e46a
Ankit Verma <ankitv78389@gmail.com>
2023-11-01 16:26:25 +0530

    :hammer:[ref][changed the website]

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,14 +1,33 @@
 import "./index.css";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 const Home = () => {
   return (
-    <div>
-      <h2>Mission</h2>Our mission is to break down language barriers and foster
-      understanding by providing top-notch translation services in a multitude
-      of Indian languages.
-      <h2>Vision</h2>
-      Our vision is to be the cornerstone of communication in India, connecting
-      businesses, communities and individuals through accurate and culturally
-      sensitive translations.
+    <div className="container">
+      <div className="home-content">
+        <h1 className="home-title">
+          Centralize your online presence with Liinks{" "}
+          <FontAwesomeIcon className="star-icon" icon={faStar} />
+        </h1>
+        <p className="home-para">
+          Gather all your links, social media, music, videos, and more in one
+          place. Create a page that matches your style and never worry about
+          changing your bio link again.
+        </p>
+        <div className="button-container">
+          <div className="home-link-icon">
+            <FontAwesomeIcon icon={faLink} />
+          </div>
+          <div className="home-link-text"></div>
+          <input className="home-input-text" placeholder="yournamehere" />
+          <div className="home-start-button">Get started</div>
+        </div>
+      </div>
+      <img
+        src="https://d1ym67wyom4bkd.cloudfront.net/assets/bundles/6da2d2ec04844202004d961a8038f3853afd3f0f/graphics/hero-graphic.png"
+        className="home-image"
+      />
     </div>
   );
 };
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,19 +1,43 @@
 import "./index.css";
-import Logo from "../../assets/logo.jpeg";
+// import Logo from "../../assets/logo.jpeg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
   return (
-    <div className="container">
-      <div className="logo-container">
-        <img className="solid-logo" src={Logo} alt="Aviral Digital Solution" />
-      </div>
-      <div className="navbar">
-        <a href="./components/Home">Home</a>
-        <a href="./components/About">About</a>
-        <a href="./components/Projects">Projects</a>
-        <a href="./components/Contact">Contact Us</a>
-      </div>
+    <div className="navbar">
+      <span>
+        <FontAwesomeIcon icon={faLink} className="img " />
+        liinks
+      </span>
+
+      <a href="./components/Home">Pricing</a>
+      <a href="./components/About">Gallery</a>
+      <a href="./components/Projects">QR Codes</a>
+      <a className="help-link" href="./components/Contact">
+        Help
+      </a>
+      <a className="right-links" href="./components/Contact">
+        Login
+      </a>
+      <a className="sign-up-link" href="./components/Contact">
+        Sign Up
+        <FontAwesomeIcon className="sign-up-icon" icon={faCirclePlus} />
+      </a>
     </div>
   );
 };
 
 export default Navbar;
+
+// <div className="container">
+// <div className="logo-container">
+//   {/* <img className="solid-logo" src={Logo} alt="Aviral Digital Solution" /> */}
+// </div>
+// <div className="navbar">
+//   <a href="./components/Home">Home</a>
+//   <a href="./components/About">About</a>
+//   <a href="./components/Projects">Projects</a>
+//   <a href="./components/Contact">Contact Us</a>
+// </div>
+// </div>
